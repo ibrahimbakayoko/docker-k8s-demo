c17b99fecc94fd8d92a814b5fb4fb9915339d766
BAKAYOKO BRAHIMA <bakayokobrahima@mbp-de-bakayoko.home>
2024-09-17 21:27:19 +0200

    first commit

---
+++ src/main/java/com/example/docker_k8s_demo/DockerK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_k8s_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerK8sDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerK8sDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker_k8s_demo/DockerK8sDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_k8s_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerK8sDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
